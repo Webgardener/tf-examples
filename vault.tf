48327c9dafea7a3e363f1a3e54e79fcb958cd52e
Thomas Weiss <Webgardener@users.noreply.github.com>
2026-07-16 16:41:39 +0200

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,1 +1,37 @@
-test
+apiVersion: kyverno.io/v1
+kind: ClusterPolicy
+metadata:
+  name: require-labels
+  annotations:
+    pod-policies.kyverno.io/autogen-controllers: none
+spec:
+  background: true
+  rules:
+  - name: apcode-and-opscontact
+    match:
+      any:
+      - resources:
+          kinds:
+          - Service
+          - Pod
+          - ReplicaSet
+          - Deployment
+          - StatefulSet
+          - DaemonSet
+          - Job
+          - CronJob
+          namespaces: [blue, shared]
+    validate:
+      failureAction: Audit
+      cel:
+        expressions:
+        - expression: >-
+            has(object.metadata.labels)
+            && 'apcode' in object.metadata.labels
+            && object.metadata.labels['apcode'].matches('^A(P[0-9]{5}|[0-9]{6})$')
+          message: "label 'apcode' requis, format APxxxxx ou Axxxxxx."
+        - expression: >-
+            has(object.metadata.labels)
+            && 'opscontact' in object.metadata.labels
+            && object.metadata.labels['opscontact'].matches('^[a-zA-Z]+.+[a-zA-Z]+_at_bnpparibas.com$')
+          message: "label 'opscontact' requis, format prenom.nom_at_bnpparibas.com."
