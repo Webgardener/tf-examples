57f4d3abe89c472d7664596c8099c3d1d1387485
Thomas Weiss <Webgardener@users.noreply.github.com>
2026-08-24 15:16:30 +0200

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -2,6 +2,14 @@ list-certs:
   stage: audit
   script:
     - |
+      to_epoch() {
+        # $1 = date openssl, ex: "Sep 12 10:32:00 2026 GMT"
+        if date -d "$1" +%s >/dev/null 2>&1; then
+          date -d "$1" +%s                                  # GNU (Linux)
+        else
+          date -j -f "%b %d %T %Y %Z" "$1" +%s              # BSD (macOS)
+        fi
+      }
       printf "%-25s | %-40s | %-25s | %-12s | %s\n" "NOM" "SUBJECT" "EXPIRATION" "JOURS REST." "STATUT"
       printf -- "-%.0s" {1..120}; echo ""
       EXPIRING=0
@@ -9,7 +17,7 @@ list-certs:
         crt=$(vault kv get -field=crt "secret/mpg/certs/$name")
         subject=$(echo "$crt" | openssl x509 -noout -subject | sed 's/^subject=//')
         enddate=$(echo "$crt" | openssl x509 -noout -enddate | cut -d= -f2)
-        end_epoch=$(date -d "$enddate" +%s)
+        end_epoch=$(to_epoch "$enddate")
         days_left=$(( (end_epoch - $(date +%s)) / 86400 ))
         if [ "$days_left" -lt 0 ]; then
           status="EXPIRE"; EXPIRING=1
